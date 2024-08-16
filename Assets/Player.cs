4b14cde32a6c796e451700570e47b7720114292a
Emil Laine <laine.emil@gmail.com>
2024-08-16 23:58:31 +0300

    add tea cup

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,10 +1,11 @@
-using System;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     public float movementSpeed;
     public float rotationSpeed;
+    [Range(0.01f, 1f)]
+    public float scaleSpeed;
 
     void Start()
     {
@@ -17,10 +18,15 @@ public class Player : MonoBehaviour
         var x = Input.GetAxis("Horizontal");
         var y = Input.GetAxis("UpDownThrust");
         var z = Input.GetAxis("Vertical");
-        transform.position += transform.rotation * (new Vector3(x, y, z) * movementSpeed * Time.fixedDeltaTime);
+        transform.position += transform.rotation * (new Vector3(x, y, z) * movementSpeed * transform.localScale.x * Time.fixedDeltaTime);
 
         var mouseX = Input.GetAxis("Mouse X");
         var mouseY = Input.GetAxis("Mouse Y");
         transform.localEulerAngles += new Vector3(-mouseY, mouseX, 0) * rotationSpeed * Time.fixedDeltaTime;
+
+        var scaleDiff = Input.GetAxis("Scale") * scaleSpeed;
+        // Debug.Log($"{scaleDiff} {Mathf.Pow(scaleDiff, Time.fixedDeltaTime)}");
+        // Camera.main.fieldOfView += scaleDiff * Time.fixedDeltaTime;
+        transform.localScale *= Mathf.Pow(1 + scaleDiff, Time.fixedDeltaTime);
     }
 }
