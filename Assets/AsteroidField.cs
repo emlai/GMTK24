9b206e331c04f4511ac24dd787083e9200d31ac5
Emil Laine <laine.emil@gmail.com>
2024-08-17 20:33:59 +0300

    add randomized asteroid placement

---
+++ Assets/AsteroidField.cs
@@ -0,0 +1,38 @@
+using UnityEditor;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class AsteroidField : MonoBehaviour
+{
+    public GameObject[] prefabs;
+    public int spawnCount;
+    public int maxSpawnDistance;
+    public float minScale;
+    public float maxScale;
+    public bool respawn;
+
+    void OnValidate()
+    {
+        if (respawn)
+        {
+            EditorApplication.delayCall += () =>
+            {
+                respawn = false;
+                var j = 0;
+                while (transform.childCount != 0)
+                {
+                    DestroyImmediate(transform.GetChild(0).gameObject);
+                    j++;
+                    if (j > 2000) break;
+                }
+
+                for (var i = 0; i < spawnCount; i++)
+                {
+                    var spawned = Instantiate(prefabs[Random.Range(0, prefabs.Length)], transform.position + Random.insideUnitSphere * maxSpawnDistance, Random.rotation);
+                    spawned.transform.localScale = Vector3.one * Random.Range(minScale, maxScale);
+                    spawned.transform.parent = transform;
+                }
+            };
+        }
+    }
+}
