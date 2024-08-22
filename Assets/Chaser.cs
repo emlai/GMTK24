52421fcf7dff6a2ea531aad0609aa10a92484e24
Emil Laine <laine.emil@gmail.com>
2024-08-22 15:00:13 +0300

    always point anglerfish at player

--- Assets/Chaser.cs
+++ Assets/Chaser.cs
@@ -18,10 +18,11 @@ public class Chaser : MonoBehaviour
 
     private void FixedUpdate()
     {
+        var lookAtTarget = Quaternion.LookRotation(targetToChase.transform.position - transform.position, targetToChase.transform.up);
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookAtTarget, turnSpeed * Time.fixedDeltaTime);
+
         if (chasing)
         {
-            var lookAtTarget = Quaternion.LookRotation(targetToChase.transform.position - transform.position);
-            transform.rotation = Quaternion.Slerp(transform.rotation, lookAtTarget, turnSpeed * Time.fixedDeltaTime);
             transform.position = Vector3.MoveTowards(transform.position, targetToChase.transform.position, chaseSpeed * Time.fixedDeltaTime);
         }
         else if (Vector3.Distance(transform.position, targetToChase.transform.position) < triggerDistance)
