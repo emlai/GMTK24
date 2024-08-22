0510959f86062b4e02188f077e72ae9a2aeb096e
Emil Laine <laine.emil@gmail.com>
2024-08-22 15:15:46 +0300

    improve pause at game start

--- Assets/Scripts/LevelStart.cs
+++ Assets/Scripts/LevelStart.cs
@@ -3,15 +3,9 @@ using UnityEngine;
 public class LevelStart : MonoBehaviour
 {
     public PauseMenu pauseMenu;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
 
-    void SlowDown()
+    void Start()
     {
-        Time.timeScale = 0.2f;
-    }
-
-	void StopTIme()
-	{
 		Time.timeScale = 0f;
-	}
+    }
 }
